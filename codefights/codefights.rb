0392442e96039aa0db561ec33133b91cc9bba4e5
Mihawk <victra.sonata@gmail.com>
2016-11-05 21:28:37 +0700

    fight again

--- codefights/codefights.rb
+++ codefights/codefights.rb
@@ -36,3 +36,20 @@ def companyBotStrategy(trainingData)
     ss/dd
 end
 
+#run some operator from string
+def arithmeticExpression(a, b, c)
+    s = ['+','-','*','/']
+    for i in 0...s.length
+        if a.to_f.send(s[i],b.to_f)==c then return true end
+    end
+    return false
+end
+#test
+For A = 2, B = 3 and C = 5, the output should be
+arithmeticExpression(A, B, C) = true.
+We can replace # with a + to obtain 2 + 3 = 5, so the answer is true.
+For A = 8, B = 2 and C = 4, the output should be
+arithmeticExpression(A, B, C) = true.
+We can replace # with a / to obtain 8 / 2 = 4, so the answer is true.
+For A = 8, B = 3 and C = 2, the output should be
+arithmeticExpression(A, B, C) = false.
